d96c1ded4ff232278cd462710777d0816dc8bac4
neorafaelfb <125259602+neorafaelfb@users.noreply.github.com>
2025-05-09 10:45:04 -0300

    v.0.2 - Menu + Uso da classe Banco

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -17,25 +19,30 @@ public class Main {
 		poupanca.imprimirExtrato();
 		*/
 		Cliente cliente = new Cliente();
-		String nomeCliente;
+		Banco banco = new Banco();
+		Scanner scanner = new Scanner(System.in);
+		List <Conta> listaContas = new ArrayList<>();
 		double valor;
 		boolean sairDoApp = false;
 		int opcao;
 
+		System.out.println("Insira o nome do banco: ");
+		banco.setNome(scanner.nextLine());
 		System.out.println("Insira o nome do cliente: ");
-		Scanner scanner = new Scanner(System.in);
-		nomeCliente = scanner.nextLine();
-		cliente.setNome(nomeCliente);
+		cliente.setNome(scanner.nextLine());
 
 		Conta cc = new ContaCorrente(cliente);
 		Conta poupanca = new ContaPoupanca(cliente);
+		listaContas.add(cc);
+		listaContas.add(poupanca);
+		banco.setContas(listaContas);
 
 		while(sairDoApp != true){
 			System.out.println("1 - Depositar CC\n2 - Sacar CC");
 			System.out.println("3 - Depositar Poupança\n4 - Sacar Poupança");
 			System.out.println("5 - Transferir CC -> Poupança\n6 - Transferir Poupança -> CC");
 			System.out.println("7 - Imprimir Extrato CC\n8 - Imprimir Extrato Poupança");
-			System.out.println("9 - Sair do programa.");
+			System.out.println("9 - Listar contas no banco\n10 - Sair do programa");
 			opcao = scanner.nextInt();
 			switch(opcao){
 				case 1:{
@@ -84,6 +91,19 @@ public class Main {
 					break;
 				}
 				case 9:{
+					List<Conta> contas = banco.getContas();
+					if (contas == null || contas.isEmpty()) {
+						System.out.println("Nenhuma conta registrada no banco.");
+					} else {
+						System.out.println("\n=== Lista de Clientes e suas Contas do Banco " + banco.getNome() + " ===");
+						for (Conta conta : contas) {
+							conta.imprimirExtrato();
+							System.out.println("--------------------------------------");
+						}
+					}
+					break;
+				}
+				case 10:{
 					System.out.println("Fim do programa.");
 					sairDoApp = true;
 					break;
